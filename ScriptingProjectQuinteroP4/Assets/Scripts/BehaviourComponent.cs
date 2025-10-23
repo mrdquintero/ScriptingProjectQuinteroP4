e2dbb64c23f7e3289eb9b1359f0898dc32df8f39
David Quintero <dquintero@dusd.net>
2025-10-23 12:19:13 -0700

    p4
    added folders

--- ScriptingProjectQuinteroP4/Assets/Scripts/BehaviourComponent.cs
+++ ScriptingProjectQuinteroP4/Assets/Scripts/BehaviourComponent.cs
@@ -13,10 +13,18 @@ public class BehaviourComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R))
+       if (Input.GetKeyDown(KeyCode.R))
         {
             GetComponent<Renderer>().material.color = Color.red;
         }
-        
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+
     }
 }
