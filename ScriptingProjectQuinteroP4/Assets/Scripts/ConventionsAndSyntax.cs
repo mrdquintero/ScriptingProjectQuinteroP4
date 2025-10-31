ff1ddcc40617d2dd44327612ad8e9c005aaab6cd
David Quintero <dquintero@dusd.net>
2025-10-31 10:45:27 -0700

    finished
    conventions and syntax

--- ScriptingProjectQuinteroP4/Assets/Scripts/ConventionsAndSyntax.cs
+++ ScriptingProjectQuinteroP4/Assets/Scripts/ConventionsAndSyntax.cs
@@ -8,21 +8,17 @@ public class ConventionsAndSyntax : MonoBehaviour
     void Start()
     {
         //this line is there to tell me the x position of my object
-
-        /*Hi there!
-         * this is two lines!
-         * */
         Debug.Log(transform.position.x);
 
-        if (transform.position.y <= 5f)
-        {
-            Debug.Log("I'm about to hit the ground!");
-        }
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        }
     }
 }
